9327c13c0b4555d498ee97ade59860c0a2425615
Dan <dnewman52@gmail.com>
2020-01-08 14:38:00 +1100

    Sample Scene
    Creating a starting point

---
+++ StealthGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 v;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponentInChildren<Rigidbody>();        
+    }
+
+
+    private void Update()
+    {
+        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        v = new Vector3(x, 0, z);
+    }
+
+    private void FixedUpdate()
+    {
+        Move(v);
+
+    }
+
+    private void Move(Vector3 v)
+    {        
+        rb.MovePosition(v);
+    }
+    
+}
