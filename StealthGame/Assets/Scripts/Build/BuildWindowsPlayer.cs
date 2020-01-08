a5d1712e5368c565cf3c6071be154b634dab56c9
Dan <dnewman52@gmail.com>
2020-01-08 23:29:39 +1100

    Attempting to get auto-builds working with gitlab
    no such luck...yet

---
+++ StealthGame/Assets/Scripts/Build/BuildWindowsPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+
+
+class BuildWindowsPlayer
+{
+    static void PerformBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "./WindowsBuild/Build.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.Development;
+        
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+   
+    }
+    
+}
